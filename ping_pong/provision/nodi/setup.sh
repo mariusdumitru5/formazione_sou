77428d6fac1781bbe2e62b8a678487affa5dd7ee
mariusdumitru5 <marius.dumitru@sourcesense.com>
2026-06-22 10:59:04 +0200

    refactor: aggiustato il file di provisioning dei nodi, c'era del codice ripetuto

--- ping_pong/provision/nodi/setup.sh
+++ ping_pong/provision/nodi/setup.sh
@@ -16,29 +16,11 @@ sh get-docker.sh
 usermod -aG docker vagrant
 
  # automazione ssh: accetta la chiave dell'orchestratore non appena viene creata
-# evita blocchi interattivi con GRUB
-export DEBIAN_FRONTEND=noninteractive
-apt-mark hold grub-pc grub-pc-bin grub2-common grub-imagebuilder 2>/dev/null
-
-# update and upgrade del sistema 
-apt-get update && apt-get upgrade -y
-
-# install di curl
-apt-get install -y curl
-
-# scarica script per installare docker 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-
-# permette all'utente vagrant di usare docker senza sudo
-usermod -aG docker vagrant
-
- # automazione ssh: accetta la chiave dell'orchestratore non appena viene creata
 if [ -f /vagrant/orchestratore_id_ed25519.pub ]; then
     mkdir -p /home/vagrant/.ssh
     touch /home/vagrant/.ssh/authorized_keys
     
-    # leggo la chiave pubblica 
+    # leggo la chiave pubblica dall cartella condivisa 
     PUB_KEY=$(cat /vagrant/orchestratore_id_ed25519.pub)
     
     # inserisco la chiave solo se non è già presente
