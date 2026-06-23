4dc146366427a82f34d1ea23a88a9d7f37de9540
Marius Dumitru <marius.dumitru@sourcesense.com>
2026-06-23 13:55:46 +0000

    feat: aggiunto script che verifica la validità di un ipv4

--- regex/checkip.sh
+++ regex/checkip.sh
@@ -9,7 +9,7 @@ RESET='\033[0m'
 # class A (0.0.0.0 - 127.255.255.2555)
 A="^([0-9]|[1-9][0-9]|1[0-2][0-7])(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}$"
 # class B (128.0.0.0 - 191.255.255.2555)
-B="^(1[2-8][0-9]|19[0-1])(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}$"
+B="^(12[8-9]|1[3-8][0-9]|19[0-1])(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}$"
 # class C (192.0.0.0 - 223.255.255.2555)
 C="^(19[2-9]|2[0-1][0-9]|22[0-3])(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}$"
 # class D (224.0.0.0 - 239.255.255.2555)
@@ -41,6 +41,55 @@ IP=""
 TYPE="Public"
 # ip class
 CLASS=""
+# netID
+NET_ID=""
+# broadcast address
+BADDR=""
+# first host
+FIRST_HOST=""
+# last host
+LAST_HOST=""
+
+to_dotted(){
+    local cidr="$1"
+    
+    if [[ ! "$cidr" =~ ^[0-9]+$ ]] || [ "$cidr" -lt 0 ] || [ "$cidr" -gt 32 ]; then
+        echo "Error: Invalid CIDR subnet mask. Must be a number between 0 and 32."
+        exit 1
+    fi
+
+    local mask=$(( (0xffffffff << (32 - cidr)) & 0xffffffff ))
+    MASK_DECIMAL="$((mask>>24 & 255)).$((mask>>16 & 255)).$((mask>>8 & 255)).$((mask & 255))"
+}
+
+
+
+# get network info about hosts, netID and broadcast address
+get_netInfo(){
+	
+	# divide the ip and the mask
+	to_cidr "$MASK" 
+	IFS=. read -r i1 i2 i3 i4 <<< "$IP"
+        IFS=. read -r m1 m2 m3 m4 <<< "$MASK_DECIMAL"
+	
+	# logic and between each octet
+        net1=$((i1 & m1))
+        net2=$((i2 & m2))
+        net3=$((i3 & m3))
+        net4=$((i4 & m4))
+        NET_ID="$net1.$net2.$net3.$net4"
+
+	b1=$(( i1 | (255 - m1) ))
+   	b2=$(( i2 | (255 - m2) ))
+    	b3=$(( i3 | (255 - m3) ))
+    	b4=$(( i4 | (255 - m4) ))
+    	BADDR="$b1.$b2.$b3.$b4"
+
+    	FIRST_HOST="$net1.$net2.$net3.$((net4 + 1))"
+    	LAST_HOST="$b1.$b2.$b3.$((b4 - 1))"			
+}
+
+# 
 
 # get comand line args
 PARS=$(getopt -o i:m:h --long ip:,mask:,help -n "$0" -- "$@")
@@ -114,8 +163,19 @@ fi
 printf "====================IP Checker====================\n"
 if [[ -n "$CLASS" ]]; then
     printf "${GREEN}[+] IP         : %s${RESET}\n" "$IP"
+    
+   #check if the mask is defined
+    if [[ -n "$MASK" ]]; then
+	get_netInfo
+        printf "${GREEN}[+] Netmask    : %s${RESET}\n" "$MASK"
+        printf "${GREEN}[+] NetID      : %s${RESET}\n" "$NET_ID"
+        printf "${GREEN}[+] Broadcast  : %s${RESET}\n" "$BADDR"
+        printf "${GREEN}[+] Host range : from %s to %s${RESET}\n" "$FIRST_HOST" "$LAST_HOST"
+	
+    fi
+   
     printf "${GREEN}[+] IP Class   : %s${RESET}\n" "$CLASS"
-    printf "${GREEN}[+] IP Type    : %s${RESET}\n" "$TYPE"
+    printf "${GREEN}[+] IP Type    : %s${RESET}\n" "$TYPE" 
     printf "==================================================\n"
     exit 0
 else
